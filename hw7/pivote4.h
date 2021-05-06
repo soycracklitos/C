e7fd7770600144d51174953d94df1ef81e4545b3
soycracklitos <78566347+soycracklitos@users.noreply.github.com>
2021-05-06 13:31:42 -0500

    Add files via upload

---
+++ hw7/pivote4.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int hourWorked(int pin);
+int registered(void);
+int assistanceCHECK(int pin);
+int timeWorked(int pin);
